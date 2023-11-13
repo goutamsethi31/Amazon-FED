29c23e90ffffc7acd4290625dd1d66740b78fb48
goutamsethi31 <goutamsethi31@gmail.com>
2023-11-13 09:58:43 +0530

    instance type

--- Main.tf
+++ Main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "Jenkins-sg" {
 
 resource "aws_instance" "web" {
   ami                    = "ami-0f5ee92e2d63afc18"
-  instance_type          = "t2.large"
+  instance_type          = "t2.medium"
   key_name               = "Mumbai"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
@@ -41,6 +41,6 @@ resource "aws_instance" "web" {
     Name = "Jenkins-sonar"
   }
   root_block_device {
-    volume_size = 30
+    volume_size = 20
   }
 }
